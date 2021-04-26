8a0895c44d4e34512edcaba2984dfee19805f9a0
Ititriin <triin.tajur@vikk.ee>
2021-04-26 18:25:37 +0300

    delete task https://github.com/Kadi12345/GreatSuccess-to-do-be/issues/12

--- src/controllers/deleteTask.controller.js
+++ src/controllers/deleteTask.controller.js
@@ -4,14 +4,13 @@ const Done = db.Done
 
 module.exports = async function (req, res) {
   try {
-    if (req.params.toTask === 'todo') {
-      const task = await Done.findOne({_id: req.params.id}).lean().exec()
-      await Done.deleteOne(task)
-    }
-    if (req.params.toTask === 'done') {
-      const task = await Todo.findOne({_id: req.params.id}).limit(100).lean().exec()
-      await Todo.deleteOne(task)
-    }
+    // TODO - Some nicer soultion should be put here (in long run it would make sense to keep all tasks in one table and keep the heading as a task parameter)
+    const done_task = await Done.findOne({_id: req.params.id}).lean().exec()
+    await Done.deleteOne(done_task)
+
+    const todo_task = await Todo.findOne({_id: req.params.id}).limit(100).lean().exec()
+    await Todo.deleteOne(todo_task)
+ 
     res.status(200).json({ message: 'Success' })
   } catch (error) {
     res.status(500).json({ message: error.message })
