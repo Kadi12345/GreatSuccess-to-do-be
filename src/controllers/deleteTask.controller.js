b9cb4076c07f5d1a14e970af3a70e85d29880b9f
Ititriin <triin.tajur@vikk.ee>
2021-04-27 11:51:48 +0300

    deleting functionality

--- src/controllers/deleteTask.controller.js
+++ src/controllers/deleteTask.controller.js
@@ -4,7 +4,6 @@ const Done = db.Done
 
 module.exports = async function (req, res) {
   try {
-    // TODO - Some nicer solution should be put here (in long run it would make sense to keep all tasks in one table and keep the heading as a task parameter)
     
     const done_task = await Done.findById({_id: req.body._id}).lean().exec()
     if(done_task !== null) {
@@ -18,8 +17,6 @@ module.exports = async function (req, res) {
 
     console.log("DONE" + done_task)
     console.log("TODO" + todo_task)
-
-
  
     res.status(200).json({ message: 'Success' })
   } catch (error) {
