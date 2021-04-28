649168d2a8acabd8f49aa908e090d2258f48acf3
Kaido24 <mumm.meelis@gmail.com>
2021-04-28 15:43:50 +0300

    added CORS

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const port = 3001
-const router = require('./src/router')
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3001;
+const router = require('./src/router');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('Hakkama said!')
-})
+  res.send('Hakkama said!');
+});
 
-app.use('/api', router)
+app.use('/api', router);
 
 app.listen(process.env.PORT || port, () => {
-  console.log("Server is running") })
+  console.log('Server is running');
+});
